178847b1f7ff23e32165be79e8f39fa94993c7c2
Mario Petkov <mario.18@abv.bg>
2021-12-26 15:32:58 +0200

    First 2 unit tests

---
+++ Forum/Forum.Test/Mocks/DatabaseMock.cs
@@ -0,0 +1,19 @@
+﻿namespace Forum.Test.Mocks
+{
+    using Forum.Data;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    public static class DatabaseMock
+    {
+        public static ApplicationDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+                return new ApplicationDbContext(dbContextOptions);
+            }
+        }
+    }
+}
---
+++ Forum/Forum.Test/Services/PostServiceTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Forum.Test.Services
+{
+    using Forum.Data.Models;
+    using Forum.Services.Post;
+    using Forum.Test.Mocks;
+    using Xunit;
+    public class PostServiceTest
+    {
+        [Fact]
+        public void DeleteShouldReturnTrue()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+            data.Posts.Add(new Post
+            {
+                Id = 1,
+                Tittle = "a",
+                Content = "b"
+            });
+            var postService = new PostService(data, null);
+            //Act
+            var isDeleted = postService.Delete(1);
+            //Assert
+            Assert.True(isDeleted);
+        }
+        [Fact]
+        public void DeleteShouldReturnFalse()
+        {
+            //Arrange
+            using var data = DatabaseMock.Instance;
+            data.Posts.Add(new Post
+            {
+                Id = 1,
+                Tittle = "a",
+                Content = "b"
+            });
+            var postService = new PostService(data, null);
+            //Act
+            var isDeleted = postService.Delete(2);
+            //Assert
+            Assert.False(isDeleted);
+        }
+    }
+}
